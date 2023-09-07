5f45129885130a1ef28dd0a9c53d019da5a16bd2
Nathan <nathanbarrosnt@gmail.com>
2023-09-07 12:19:46 -0300

    buttons and reactivity

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -22,4 +22,12 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Linked on card.component.html
+  add() {
+    this.products.push("New product")
+  }
+  // Linked on card.component.html
+  remove() {
+    this.products.pop()
+  }
 }
